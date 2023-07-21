53de50a0c7579d3bc83009b538475bb2c03e614e
josphat <mjirura0707@gmail.com>
2023-07-21 15:51:20 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,9 @@ function NavBar() {
 
   return <nav>
 
-  <a href="#home">home</a>
-  <a href="#about">about</a>
-  <a href="#projects">projects</a>
+  <a href="#home" key = "home">home</a>
+  <a href="#about" key="about">about</a>
+  <a href="#projects" key="projects">projects</a>
   
   </nav>;
 }
